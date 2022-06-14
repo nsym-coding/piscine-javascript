c788c543ba28ee78700576847bd72fe97e0719f6
nsym_coding <nsym_coding@noreply.learn.01founders.co>
2022-06-14 13:48:41 +0000

    Update 'match-cron.js'

--- match-cron.js
+++ match-cron.js
@@ -1,24 +1,30 @@
  function matchCron(cron, date){
     var inputDate = new Date(date)
-    var dayOfWeek = inputDate.getDay().toString()
-    var monthOfYear = (inputDate.getMonth() + 1).toString()
+    var dayOfWeek = inputDate.getDay()
+    var monthOfYear = (inputDate.getMonth() + 1)
     var dayOfMonth = (inputDate.getUTCDate() + 1)
-    var hourOfDay = (inputDate.getHours().toString())
+    var hourOfDay = (inputDate.getHours())
 
-
+  console.log(typeof dayOfMonth,dayOfMonth);
 
     
 
     if (inputDate.getDay() === 0){
         dayOfWeek = 7
-        dayOfWeek = dayOfWeek.toString()
+        
     }
 
-    var arrCron = cron.split(" ")  
+    var arrCron = cron.split(" ")
 
+    var res = false
+
+    
+    console.log("checking day",inputDate.getDay());
+    console.log("new day", dayOfWeek);
+    console.log(arrCron[4]);
    
     
-    if ((arrCron[0] === inputDate.getMinutes().toString() || arrCron[0] === "*") && (arrCron[1] === hourOfDay || arrCron[1] === "*") && (arrCron[2] == dayOfMonth || arrCron[2] === "*")  && (arrCron[3] === monthOfYear || arrCron[3] === "*") && (arrCron[4] === dayOfWeek || arrCron[4] === "*")){
+    if ((arrCron[0] == inputDate.getMinutes().toString() || arrCron[0] == "*") && (arrCron[1] == hourOfDay || arrCron[1] == "*") && (arrCron[2] == dayOfMonth || arrCron[2] == "*")  && (arrCron[3] == monthOfYear || arrCron[3] == "*") && (arrCron[4] == dayOfWeek || arrCron[4] == "*")){
       return true
     }else{
         return false
