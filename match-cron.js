74985659cc5d94bf4227311431f77ce5d9d622f3
nsym_coding <nsym.coding@outlook.com>
2022-06-14 14:47:19 +0100

    match cron

--- match-cron.js
+++ match-cron.js
@@ -2,10 +2,10 @@ function matchCron(cron, date){
     var inputDate = new Date(date)
     var dayOfWeek = inputDate.getDay().toString()
     var monthOfYear = (inputDate.getMonth() + 1).toString()
-    var dayOfMonth = (inputDate.getUTCDate() + 1).toString()
+    var dayOfMonth = (inputDate.getUTCDate() + 1)
     var hourOfDay = (inputDate.getHours().toString())
 
-  console.log(hourOfDay);
+  console.log(typeof dayOfMonth,dayOfMonth);
 
     
 
@@ -24,7 +24,7 @@ function matchCron(cron, date){
     console.log(arrCron[4]);
    
     
-    if ((arrCron[0] === inputDate.getMinutes().toString() || arrCron[0] === "*") && (arrCron[1] === hourOfDay || arrCron[1] === "*") && (arrCron[2] === dayOfMonth || arrCron[2] === "*")  && (arrCron[3] === monthOfYear || arrCron[3] === "*") && (arrCron[4] === dayOfWeek || arrCron[4] === "*")){
+    if ((arrCron[0] == inputDate.getMinutes().toString() || arrCron[0] == "*") && (arrCron[1] == hourOfDay || arrCron[1] == "*") && (arrCron[2] == dayOfMonth || arrCron[2] == "*")  && (arrCron[3] == monthOfYear || arrCron[3] == "*") && (arrCron[4] == dayOfWeek || arrCron[4] == "*")){
       return true
     }else{
         return false
