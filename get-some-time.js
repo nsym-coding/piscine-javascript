812a1663f68b5e6af53a31fed00ee0509af37ef6
nsym_coding <nsym.coding@outlook.com>
2022-06-14 12:48:18 +0100

    get some time

---
+++ get-some-time.js
@@ -0,0 +1,38 @@
+function firstDayWeek(week, year){
+
+    var inputDate = new Date (Date.UTC(year, 0, 1 + (week - 1) * 7, 12));
+    
+    if (year < 1000){
+
+        inputDate.setFullYear(year)
+    }
+    var weekStart = inputDate;
+
+    if (week === 1 && year === "1000"){
+        weekStart = new Date(Date.UTC(1000, 0, 1, 12))
+        
+    }else{
+
+        while (inputDate.getDay() != 1){
+              weekStart.setDate(inputDate.getDate() - 1)
+          }
+        
+    }
+
+   
+
+
+
+    var resDay = weekStart.getDate().toString().padStart(2, 0);
+    var resMonth = (weekStart.getMonth()+1).toString().padStart(2,0);
+    var resYear = weekStart.getUTCFullYear().toString().padStart(4,0);
+    
+
+    var result = resDay + "-" + resMonth + "-" + resYear
+
+    // for (i = weekStart.length; i>0; i--){
+    //     result += weekStart[i]
+    // }
+
+    return result
+}
