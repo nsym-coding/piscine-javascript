e8ebd4374c503dff6887cf0f09e7b5bf14c8b825
nsym_coding <nsym_coding@noreply.learn.01founders.co>
2022-06-14 13:45:48 +0000

    Update 'match-cron.js'

--- match-cron.js
+++ match-cron.js
@@ -2,7 +2,7 @@
     var inputDate = new Date(date)
     var dayOfWeek = inputDate.getDay().toString()
     var monthOfYear = (inputDate.getMonth() + 1).toString()
-    var dayOfMonth = (inputDate.getUTCDate() + 1).toString()
+    var dayOfMonth = (inputDate.getUTCDate() + 1)
     var hourOfDay = (inputDate.getHours().toString())
 
 
@@ -18,7 +18,7 @@
 
    
     
-    if ((arrCron[0] === inputDate.getMinutes().toString() || arrCron[0] === "*") && (arrCron[1] === hourOfDay || arrCron[1] === "*") && (arrCron[2] === dayOfMonth || arrCron[2] === "*")  && (arrCron[3] === monthOfYear || arrCron[3] === "*") && (arrCron[4] === dayOfWeek || arrCron[4] === "*")){
+    if ((arrCron[0] === inputDate.getMinutes().toString() || arrCron[0] === "*") && (arrCron[1] === hourOfDay || arrCron[1] === "*") && (arrCron[2] == dayOfMonth || arrCron[2] === "*")  && (arrCron[3] === monthOfYear || arrCron[3] === "*") && (arrCron[4] === dayOfWeek || arrCron[4] === "*")){
       return true
     }else{
         return false
