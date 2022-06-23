f327a0667e26fcef94b8024f5bc90beb51c3719d
nsym_coding <nsym.coding@outlook.com>
2022-06-23 19:10:16 +0100

    gossip grid

--- gossip-grid.js
+++ gossip-grid.js
@@ -13,21 +13,22 @@ export const grid = () => {
      txtArea.id = 'textarea'
      txtArea.setAttribute('placeholder', 'Got a gossip to share ?')
      submitButton.setAttribute('type', 'button')
-     submitButton.setAttribute('class', 'button')
+     //submitButton.setAttribute('class', 'button')
      submitButton.textContent = 'Share gossip!'
-     form.setAttribute('id', 'myForm')
-     form.appendChild(txtArea)
-     form.appendChild(submitButton)
-     document.body.append(form)
+    //  form.setAttribute('id', 'myForm')
+    //  form.appendChild(txtArea)
+    //  form.appendChild(submitButton)
+     //document.body.append(form)
 
      //event listener to create new gossip card on submit click
-     submitButton.addEventListener('mousedown', function(){
+     submitButton.addEventListener('mousedown', function(e){
        
         let gsp = document.createElement('div')
         gsp.setAttribute('class', 'gossip')
         gsp.textContent = txtArea.value.toString()
-        document.body.insertBefore(gsp,document.body.firstElementChild)
+        document.body.insertBefore(gsp,document.querySelectorAll('.gossip')[1])
         txtArea.value = ""
+        e.preventDefault()
         //console.log(txtArea.value.toString());
         // document.querySelectorAll('.gossip')[1]
 })
@@ -36,10 +37,12 @@ export const grid = () => {
    for (let i =0; i<gossips.length; i++){
     //condition to create first card as a form
        if (i === 0){
-           gossip = document.createElement('div')
-           gossip.appendChild(form)
+           gossip = document.createElement('form')
+           gossip.appendChild(txtArea)
+           gossip.appendChild(submitButton)
+
            gossip.setAttribute('class', 'gossip')
-           gossip.setAttribute('id', 'gossip')
+           //gossip.setAttribute('id', 'gossip')
            
            
            document.body.append(gossip)
@@ -48,7 +51,7 @@ export const grid = () => {
         //creating the rest of the cards as normal
         gossip = document.createElement('div')
         gossip.setAttribute('class', 'gossip')
-        gossip.setAttribute('id', 'gossip')
+        //gossip.setAttribute('id', 'gossip')
         gossip.setAttribute('name', 'gossip')
         gossip.innerHTML = gossips[i].toString()
         document.body.append(gossip)
@@ -64,18 +67,18 @@ export const grid = () => {
     var fontSizeSlider = document.createElement('input')
     var backgroundSlider = document.createElement('input')
 
-    //setting labels for the 3 range inputs
-    const widthLabel = document.createElement("LABEL");
-    widthLabel.setAttribute("for", '.width');
-    widthLabel.innerHTML = "width";
+    // //setting labels for the 3 range inputs
+    // const widthLabel = document.createElement("LABEL");
+    // widthLabel.setAttribute("for", '.width');
+    // widthLabel.innerHTML = "width";
 
     //setting attributes for width range
     widthSlider.id = 'width'
     widthSlider.type = 'range'
     widthSlider.className = 'range'
-    widthSlider.setAttribute('min', 200)
-    widthSlider.setAttribute('max',800)
-    widthSlider.defaultValue = 200
+    widthSlider.setAttribute('min', '200')
+    widthSlider.setAttribute('max','800')
+    //widthSlider.defaultValue = ''
     
     
     
@@ -83,7 +86,7 @@ export const grid = () => {
     fontSizeSlider.id = 'fontSize'
     fontSizeSlider.type = 'range'
     fontSizeSlider.className = 'range'
-    fontSizeSlider.defaultValue = '25'
+    //fontSizeSlider.defaultValue = ''
     fontSizeSlider.setAttribute('min', '20')
     fontSizeSlider.setAttribute('max', '40')
 
@@ -92,7 +95,7 @@ export const grid = () => {
     backgroundSlider.id = 'background'
     backgroundSlider.type = 'range'
     backgroundSlider.className = 'range'
-    backgroundSlider.defaultValue = '60'
+    //backgroundSlider.defaultValue = ''
     backgroundSlider.setAttribute('min', '20')
     backgroundSlider.setAttribute('max', '75')
 
@@ -102,36 +105,44 @@ export const grid = () => {
     ranges.appendChild(widthSlider)
     ranges.appendChild(fontSizeSlider)
     ranges.appendChild(backgroundSlider)
-    ranges.insertBefore(widthLabel, document.querySelectorAll('.range')[0])
-    //appending labels to body
-    document.body.append(widthLabel)
+    
     //appending ranges to the body
-    document.body.append(ranges)
+    document.body.appendChild(ranges)
     
     let arrGoss = document.querySelectorAll('.gossip')
     
     widthSlider.addEventListener('input', function(e){
         
 
-        for (let i =0; i<arrGoss.length; i++){
-            arrGoss[i].style.width = e.target.value + 'px'
+        
             
-        }
+          let dvs =  document.querySelectorAll('.gossip')
+            dvs.forEach((x) => x.style.width = e.target.value + 'px')
+            
+     
     });
 
    fontSizeSlider.addEventListener('input', function(e){
-    for (let i =0; i<arrGoss.length; i++){
-        arrGoss[i].style.fontSize = e.target.value + 'px'
+    
+    
+    let dvs =  document.querySelectorAll('.gossip')
+    dvs.forEach((x) => x.style.fontSize = e.target.value + 'px')
         
-    }
+   
    })
 
    backgroundSlider.addEventListener('input', function(e){
-    for (let i =0; i<arrGoss.length; i++){
-        arrGoss[i].style.backgroundColor = `hsl(280, 50%, ${e.target.value}%)`
+
+    let dvs =  document.querySelectorAll('.gossip')
         
-    }
+        dvs.forEach((x) => x.style.backgroundColor = `hsl(280, 50%, ${e.target.value}%)` )
+   
+        
+        
+    
    
     
    })
 }
+
+
